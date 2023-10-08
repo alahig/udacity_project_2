7e8778efad5480f2c8803b02ca8d814b92e10ddb
unknown <alvaro_lahiguera@hotmail.com>
2023-10-08 15:14:13 +0200

    Completed data process pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,62 @@
 import sys
+import pandas as pd
+import numpy as np
+import os
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    #- Merge the messages and categories datasets using the common id
+    #- Assign this combined dataset to `df`, which will be cleaned in the following steps
+
+    return messages, categories
+
+def clean_data(messages, categories):
+
+    #- Split the values in the `categories` column on the `;` character so that each value becomes a separate column. You'll find [this method](https://pandas.pydata.org/pandas-docs/version/0.23/generated/pandas.Series.str.split.html) very helpful! Make sure to set `expand=True`.
+    #- Use the first row of categories dataframe to create column names for the categories data.
+    #- Rename columns of `categories` with new column names.
+
+        
+    def convert_feature_list(li):
+        ''' Converts a string in the format 
+            'related-1;request-0;offer-0;
+            to a dict where the category names are the keys 
+            and the values are the integers.
+            i.e. {'related': 1, 'request': 0, 'offer': 0}
+            
+        '''
+        elements = li.split(';')
+        return {k.split('-')[0]: int(k.split('-')[-1]) for k in elements}
+
+    categories = pd.DataFrame({i: convert_feature_list(v) for i, v in categories.items()}).T
+
+    # There are a few entries where the related value is 2. This should be considered a dataerror
+    # i.e. 
+    # categories.loc[4145]
+    # 'related-2;request-0;offer-0;aid_related-0;medical_help-0;medical_products-0;search_and_rescue-0;security-0;military-0;child_alone-0;water-0;food-0;shelter-0;clothing-0;money-0;missing_people-0;refugees-0;death-0;other_aid-0;infrastructure_related-0;transport-0;buildings-0;electricity-0;tools-0;hospitals-0;shops-0;aid_centers-0;other_infrastructure-0;weather_related-0;floods-0;storm-0;fire-0;earthquake-0;cold-0;other_weather-0;direct_report-0'
+    categories = categories.clip(upper=1)
 
 
-def clean_data(df):
-    pass
+    #Merge the data
+    df = messages.merge(categories, how='left', left_on='id', right_index=True)
 
+    ### 6. Remove duplicates.
+
+   
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    # check number of duplicates
+    assert not(df.duplicated().any())
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    ### Save the clean dataset into an sqlite database.
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('Messages', engine, index=False, if_exists='replace')  
 
 
 def main():
@@ -20,10 +66,10 @@ def main():
 
         print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
               .format(messages_filepath, categories_filepath))
-        df = load_data(messages_filepath, categories_filepath)
+        messages, categories = load_data(messages_filepath, categories_filepath)
 
         print('Cleaning data...')
-        df = clean_data(df)
+        df = clean_data(messages, categories)
         
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
